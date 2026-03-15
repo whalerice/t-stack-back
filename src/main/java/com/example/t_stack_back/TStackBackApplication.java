d987ec9e5566bc71843ed18ade093ab6bd24a892
joy <joyfulforest@gmail.com>
2026-03-15 16:13:03 +0900

    first commit

---
+++ src/main/java/com/example/t_stack_back/TStackBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.t_stack_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TStackBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TStackBackApplication.class, args);
+	}
+
+}
